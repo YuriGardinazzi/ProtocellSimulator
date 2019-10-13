c80346cdcec6aa118d1241e130a10c9572c2f49b
yuri <gardinazzi.yuri@gmail.com>
2019-10-13 17:58:40 +0200

    add random movement to cells

--- proto_test/src/my_growth.h
+++ proto_test/src/my_growth.h
@@ -19,7 +19,17 @@ namespace bdm{
         void Run(SimObject* so) override {
             if (auto* cell = dynamic_cast<MyCell*>(so)) {
             if (cell->GetDiameter() < 8) {
+                auto* random = Simulation::GetActive()->GetRandom();
+                // Here 400 is the speed and the change to the volume is based on the
+                // simulation time step.
+                // The default here is 0.01 for timestep, not 1.
                 cell->ChangeVolume(400);
+
+                // create an array of 3 random numbers between -2 and 2
+                Double3 cell_movements = random->template UniformArray<3>(-2, 2);
+                // update the cell mass location, ie move the cell
+                cell->UpdatePosition(cell_movements);
+                        
             }
             else {
                 cell->Divide();
