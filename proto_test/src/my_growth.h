3b1d2e511657311c33a863ba19b763238192afa3
YuriGardinazzi <gardinazzi.yuri@gmail.com>
2019-10-29 20:37:36 +0100

    add (WIP) random cell removal

--- proto_test/src/my_growth.h
+++ proto_test/src/my_growth.h
@@ -18,23 +18,36 @@ namespace bdm{
 
         void Run(SimObject* so) override {
             if (auto* cell = dynamic_cast<MyCell*>(so)) {
-            if (cell->GetDiameter() < 8) {
-                auto* random = Simulation::GetActive()->GetRandom();
-                // Here 400 is the speed and the change to the volume is based on the
-                // simulation time step.
-                // The default here is 0.01 for timestep, not 1.
-                cell->ChangeVolume(400);   
-                
-                 
-                // create an array of 3 random numbers between -2 and 2
-                Double3 cell_movements = random->template UniformArray<3>(-2, 2);
-                // update the cell mass location, ie move the cell
-                cell->UpdatePosition(cell_movements);
-                        
-            }
-            else {
-                cell->Divide();
-            }
+				
+				if (cell->GetDiameter() < 8) {
+					auto* random = Simulation::GetActive()->GetRandom(); 
+					// Here 400 is the speed and the change to the volume is based on the
+					// simulation time step.
+					// The default here is 0.01 for timestep, not 1.
+					cell->ChangeVolume(400);   
+						
+						
+					// create an array of 3 random numbers between -2 and 2
+					Double3 cell_movements = random->template UniformArray<3>(-2, 2);
+					// update the cell mass location, ie move the cell
+					cell->UpdatePosition(cell_movements);
+								
+					}else {
+					/*	//FIXME: 
+					 	//at the moment the core dumps while running the simulation with
+						//this snippet of code
+						//TODO: find a more effective way to delete cells randomly
+						auto* rm = Simulation::GetActive()-> GetResourceManager();
+						int rand = std::rand() % 100;
+						
+						if (rand <80){
+							
+							rm->Remove(so->GetUid());
+						}else{
+							cell->Divide();
+						}*/
+						cell ->Divide();
+					}
             }
         }
 
