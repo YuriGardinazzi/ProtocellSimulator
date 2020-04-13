b4d9e9242d7a320cc248f80c1af81547aa549c78
yuri <gardinazzi.yuri@gmail.com>
2020-04-13 17:57:07 +0200

    initial commit

--- elementare_7/build/CMakeFiles/check-tidy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/check-tidy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/check-tidy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- elementare_7/build/CMakeFiles/show-format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/show-format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/show-format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
