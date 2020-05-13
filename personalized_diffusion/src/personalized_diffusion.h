f4f35bef9aa16d87da50a30d0e497acd5db4aec8
YuriGardinazzi <gardinazzi.yuri@gmail.com>
2020-05-13 11:12:27 +0200

    add diffusion

--- personalized_diffusion/src/personalized_diffusion.h
+++ personalized_diffusion/src/personalized_diffusion.h
@@ -19,15 +19,45 @@
 
 namespace bdm {
 
+
+
 inline int Simulate(int argc, const char** argv) {
+  
+  //Initialize Biodynamo
   Simulation simulation(argc, argv);
 
   // Define initial model - in this example: single cell at origin
   auto* rm = simulation.GetResourceManager();
-  rm->push_back(new Cell(30));
 
-  // Run simulation for one timestep
-  simulation.GetScheduler()->Simulate(1);
+
+  auto construct = [](const Double3& position) {
+    Cell* cell = new Cell(position);
+    cell->SetDiameter(30);
+    cell->SetMass(1.0);
+    //cell->AddBiologyModule(new Chemotaxis());
+    Double3 secretion_position = {{50, 50, 50}};
+    if (position == secretion_position) {
+      cell->AddBiologyModule(new ASecretion());
+    }
+    return cell;
+  };
+
+  std::vector<Double3> positions;
+  positions.push_back({0, 0, 0});
+  positions.push_back({100, 0, 0});
+  positions.push_back({0, 100, 0});
+
+  // The cell responsible for secretion
+  positions.push_back({50, 50, 50});
+
+  ModelInitializer::CreateCells(positions, construct);
+  // Define the substances that cells may secrete
+  ModelInitializer::DefineSubstance(Aspecie, "A", 0.4, 0, 25);
+
+
+
+  // Run simulation 
+  simulation.GetScheduler()->Simulate(1000);
 
   std::cout << "Simulation completed successfully!" << std::endl;
   return 0;
--- personalized_diffusion/src/personalized_models.h
+++ personalized_diffusion/src/personalized_models.h
@@ -6,6 +6,27 @@
 #include "biodynamo.h"
 
 namespace bdm{
+enum Substances{ Aspecie };
+
+
+
+//Definition of the secretion behaviour
+
+struct ASecretion : public BaseBiologyModule{
+  
+  BDM_STATELESS_BM_HEADER(ASecretion, BaseBiologyModule, 1);
+
+  ASecretion() : BaseBiologyModule() {}
+
+  void Run(SimObject* so) override {
+      auto* sim = Simulation::GetActive();
+      auto* rm = sim->GetResourceManager();
+      static auto* aDg = rm->GetDiffusionGrid(Aspecie);
+      double amount = 4;
+      aDg->IncreaseConcentrationBy(so->GetPosition(), amount);
+  }
+
+};
 
 }
 
