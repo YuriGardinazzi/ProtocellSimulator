f85a5109aa1d99ee71e9946d6cf80ab6e36a97ce
yuri <gardinazzi.yuri@gmail.com>
2020-05-21 23:04:18 +0200

    update read csv

--- elementare_8/read_csv.py
+++ elementare_8/read_csv.py
@@ -2,9 +2,17 @@ import csv
 
 import matplotlib.pyplot as plt
 
+id = set()
 table = {}
-with open('volume.csv','r') as file:
-    reader = csv.reader(file)
+with open('volume.csv') as file:
+    reader = csv.reader(file, delimiter=';')
     for row in reader:
-        table[row[0]] = row[1:]
-print(table)
+        if not row[0] in id:
+            id.add(row[0])
+            table[row[0]] = {'x':[], 'y':[]}
+        table[row[0]]['x'].append(row[1])
+        table[row[0]]['y'].append(row[2])
+for key in table:
+    with open(key+".csv",'w') as file:
+        for i in range(len(table[key]['x'])):
+                file.write(table[key]['x'][i]+";"+table[key]['y'][i]+"\n")
